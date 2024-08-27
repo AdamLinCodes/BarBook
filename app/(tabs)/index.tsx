755d6d08a3ee1976faa43676adece9845514ff37
Adam Lin <andrewhlin0@gmail.com>
2024-08-27 13:58:41 -0400

    fleshed out home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,6 +6,11 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+
+  const openLink = (url: string) => {
+    Linking.openURL(url).catch(err => console.error("Failed to open URL:", err));
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -21,6 +26,29 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedText type="subtitle">Welcome back to your recipe book</ThemedText>
+
+      <ThemedText type='default'>
+        Welcome back to your own personal Barbook, filled with over a hundred cocktail 
+        recipes. This app uses TheCocktailDB API for all its information and is built 
+        using React Native. If you want to learn more about the developer or contribute 
+        to the code, below I’ve linked my LinkedIn as well as the source code for this 
+        project. Cheers everyone <ThemedText type='title'>🍻</ThemedText>
+      </ThemedText>
+
+      <ThemedView style={styles.linkContainer}>
+        <TouchableOpacity onPress={() => openLink('https://www.linkedin.com/in/adam-lin-7314ab19a/')}>
+          <Image
+            source={require('@/assets/images/linkedin.png')}
+            style={styles.link}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openLink('https://github.com/AdamLinCodes/BarBook')}>
+          <Image
+            source={require('@/assets/images/github.png')}
+            style={styles.link}
+          />
+        </TouchableOpacity>
+      </ThemedView>
 
     </ParallaxScrollView>
   );
@@ -43,4 +71,12 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  linkContainer: {
+    flexDirection: 'row', // Aligns children (images) in a row
+  },
+  link: {
+    height:75,
+    width:75,
+    marginHorizontal: 10,
+  }
 });
